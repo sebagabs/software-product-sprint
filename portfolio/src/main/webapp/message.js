053c8bda8b2ee7a6564925930a6c93c73ec4bb86
Sebastian Maldonado <sg.maldonador01@gmail.com>
2021-03-05 18:25:02 +0000

    Delete message.js file and use script.js file instead

--- portfolio/src/main/webapp/message.js
+++
@@ -1,7 +0,0 @@
-async function displayMessage() {
-    const responseFromServer = await fetch('"/fetch-hello-world-string"');
-    const textFromResponse = await responseFromServer.text();
-
-    const dateContainer = document.getElementById('message-cont');
-    dateContainer.innerText = textFromResponse;
-}
