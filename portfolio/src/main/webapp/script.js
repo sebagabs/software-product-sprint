113435b3784535ee6ce441f869d6ef1ffad756f3
Sebastian Maldonado <sg.maldonador01@gmail.com>
2021-03-05 18:35:28 +0000

    Fix use of quotation marks in const inside displayMessage() func in script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -30,7 +30,7 @@ function addRandomGreeting() {
 }
 
 async function displayMessage() {
-    const responseFromServer = await fetch('"/fetch-hello-world-string"');
+    const responseFromServer = await fetch('/fetch-hello-world-string');
     const textFromResponse = await responseFromServer.text();
 
     const dateContainer = document.getElementById('message-cont');
