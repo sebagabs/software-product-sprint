889025c3eb4d344a41a85f26624b460a164fa154
Sebastian Maldonado <sg.maldonador01@gmail.com>
2021-03-05 18:23:56 +0000

    Add JavaScript function from message.js in script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,3 +28,11 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function displayMessage() {
+    const responseFromServer = await fetch('"/fetch-hello-world-string"');
+    const textFromResponse = await responseFromServer.text();
+
+    const dateContainer = document.getElementById('message-cont');
+    dateContainer.innerText = textFromResponse;
+}
