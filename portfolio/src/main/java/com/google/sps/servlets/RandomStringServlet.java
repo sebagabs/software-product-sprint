3dd69c326f72d9fcdc1c10a8736b751acad68e86
Sebastian Maldonado <sg.maldonador01@gmail.com>
2021-03-05 20:21:41 +0000

    Add String[] stringCollection object and transform it into JSON string

--- portfolio/src/main/java/com/google/sps/servlets/RandomStringServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/RandomStringServlet.java
@@ -5,14 +5,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 @WebServlet("/fetch-hello-world-string")
 public class RandomStringServlet extends HttpServlet {
 
+    public String[] stringCollection = {"4 is the cosmic number.","This is a random message",
+    "I have no idea of what to write here."};
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html;");
-        response.getWriter().println("<p>hello world</p>");
+        //response.setContentType("text/html;");
+        //response.getWriter().println("<p>hello world</p>");
+        Gson gson = new Gson();
+        String json = gson.toJson(stringCollection);
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+
     }
 
 }
